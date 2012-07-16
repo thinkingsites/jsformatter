200c0ff1be9f092868263279b7f943a5ec09e65b
Miguel Ludert <github@thinkingsitesinc.com>
2012-07-16 11:15:21 -0400

    Adding GPL License

--- src/string.format.1.0.0.js
+++ src/string.format.1.0.0.js
@@ -1,4 +1,24 @@
-// http://msdn.microsoft.com/en-us/library/26etazsy
+/*
+JSFormatter - created by Miguel Ludert
+https://github.com/thinkingsites/jsformatter
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+This package replicates the .Net formatting functionality.  
+For full usage documentation refer here:
+http://msdn.microsoft.com/en-us/library/26etazsy
+*/
 (function(){
 	"use strict";
 	if(typeof String.prototype.format === "undefined")
